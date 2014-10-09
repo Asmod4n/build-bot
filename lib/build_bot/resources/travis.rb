43d47f171dc7f3292a76c16cb2cae944800d589f
Hendrik Beskow <hendrik-github2@beskow.de>
2014-10-09 16:16:52 +0200

    try notifications again

---
+++ lib/build_bot/resources/travis.rb
@@ -0,0 +1,19 @@
+﻿require_relative '../resource'
+require 'uri'
+require 'multi_json'
+
+module BuildBot
+  module Resources
+    class Travis < Resource
+      def allowed_methods
+        [:POST.to_s]
+      end
+
+      def process_post
+        body = URI.decode_www_form(request.body.to_s)
+        puts MultiJson.load(body['payload'])
+        true
+      end
+    end
+  end
+end
