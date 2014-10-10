7f597b9be1e2c001cfdab0997128d9987d79ea62
Asmod4n <Asmod4n@users.noreply.github.com>
2014-10-10 12:11:14 +0200

    check integrity of libsodium

--- install_libsodium.sh
+++ install_libsodium.sh
@@ -1,7 +1,10 @@
 ﻿#!/bin/sh
 set -ex
+gpg --recv-keys 1CDEA439
 curl -O https://download.libsodium.org/libsodium/releases/libsodium-1.0.0.tar.gz
+curl -O https://download.libsodium.org/libsodium/releases/libsodium-1.0.0.tar.gz.sig
+gpg --verify libsodium-1.0.0.tar.gz.sig || exit
 tar xzf libsodium-1.0.0.tar.gz
 cd libsodium-1.0.0/
 ./configure
-make && sudo make install && sudo ldconfig
+make check && sudo make install && sudo ldconfig
